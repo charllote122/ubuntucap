b0d699bc35a77649da61fdbd81aa20483059e2c3
sofi Chemjor <sofiachemjor@gmail.com>
2025-11-12 18:33:51 +0000

    Initial commit

--- UbuntuCap/backend/ubuntucap/settings/development.py
+++ UbuntuCap/backend/ubuntucap/settings/development.py
@@ -11,3 +11,14 @@ INSTALLED_APPS += [
 
 # Email backend for development
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# CSRF settings for development
+CSRF_TRUSTED_ORIGINS = [
+    'https://humble-parakeet-7vprgxgxq7qxf79q-8001.app.github.dev',
+    'http://localhost:8001',
+    'https://localhost:8001',
+]
+
+# Cookie settings for development
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
